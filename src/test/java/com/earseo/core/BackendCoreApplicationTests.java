c65ec9b9f66c05045c3f67ac37096e95dddfeb04
yoonho <nam2660433@gmail.com>
2025-12-01 04:15:48 +0900

    [#2] test: test 설정

--- src/test/java/com/earseo/core/BackendCoreApplicationTests.java
+++ src/test/java/com/earseo/core/BackendCoreApplicationTests.java
@@ -1,12 +1,26 @@
 package com.earseo.core;
 
+import com.google.cloud.texttospeech.v1.TextToSpeechClient;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class BackendCoreApplicationTests {
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        @Primary
+        public TextToSpeechClient textToSpeechClient() {
+            return Mockito.mock(TextToSpeechClient.class);
+        }
+    }
 
     @Test
     void contextLoads() {
